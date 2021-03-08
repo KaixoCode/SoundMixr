859d556d52f0ddeddb2023958ecaa4a415d281b0
Justin Ruiter | s2264609 <j.a.g.ruiter@student.utwente.nl>
2021-03-08 14:20:09 +0100

    Soundboard things uncommented

--- source/ui/Soundboard.cpp
+++ source/ui/Soundboard.cpp
@@ -1,7 +1,7 @@
 #include "ui/Soundboard.hpp"
 
 Soundboard::Soundboard()
-    : Frame("Soundboard", 500, 500, true, true, false)
+    : Frame("Soundboard", 1000, 1000, true, true, false)
 {
     namespace G = ButtonGraphics; namespace BT = ButtonType; namespace MG = MenuGraphics; namespace MT = MenuType;
 
