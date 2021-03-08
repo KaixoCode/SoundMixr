76ed7b3ba4198b61c8c08b6a24d6277f1fdedfa6
Justin Ruiter | s2264609 <j.a.g.ruiter@student.utwente.nl>
2021-03-08 15:16:22 +0100

    Downsampling + stop playback

--- include/ui/Soundboard.hpp
+++ include/ui/Soundboard.hpp
@@ -15,7 +15,9 @@ public:
 		if (m_SampleNum < 0)
 			return 0;
 
-		if (m_SampleNum == m_File.getNumSamplesPerChannel())
+		int curSample = std::floor(m_SampleNum * m_MultiplicationFactor);
+
+		if (curSample == m_File.getNumSamplesPerChannel())
 		{
 			m_SampleNum = -1;
 			return 0;
@@ -24,7 +26,7 @@ public:
 		if (m_SampleNum >= 0 && channel == 0)
 			m_SampleNum++;
 
-		return m_File.samples[channel][m_SampleNum];
+		return m_File.samples[channel][curSample];
 	}
 
 	void LoadFile(const std::string& path) { m_Filepath = path; }
@@ -32,8 +34,11 @@ public:
 	{
 		if (m_File.getNumSamplesPerChannel() > 0) 
 		{
-			// A file is already loaded, play it
-			m_SampleNum = 0;
+			// A file is already loaded, play it if it isn't playing
+			if (m_SampleNum < 0)
+				m_SampleNum = 0;
+			else
+				m_SampleNum = -1;
 		}
 		else 
 		{
@@ -57,6 +62,7 @@ public:
 
 			m_Filepath = fileNameStr;
 			m_File.load(fileNameStr);
+			m_MultiplicationFactor = (m_File.getSampleRate() / 48000.0);
 
 			// Set the name of the button to the filename
 			std::filesystem::directory_entry loadedFile{ fileNameStr };
@@ -68,6 +74,7 @@ private:
 	std::string m_Filepath;
 	AudioFile<double> m_File;
 	int m_SampleNum = -1;
+	float m_MultiplicationFactor = 1.0F;
 };
 
 class Soundboard : public Frame
