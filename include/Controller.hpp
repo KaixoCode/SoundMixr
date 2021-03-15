82aad86607ce9d8c24874af8b87ff0d369209876
KaixoCode <jeroenzwiers2@gmail.com>
2021-03-15 21:41:07 +0100

    Update Controller.hpp

--- include/Controller.hpp
+++ include/Controller.hpp
@@ -6,17 +6,6 @@
 #include "ui/Soundboard.hpp"
 #include "ui/Graphics.hpp"
 
-/*
- * TODO:
- *  - Add sound visuals
- *  - Option to make GUI vertical
- *  - Individual routing volume
- *  - Option to disable clipping
- *  - Audio limiter to prevent clipping
- *  - 'audio test' mode, notify when input hasn't received audio in a while.
- *  x Fix laggy text rendering
- */
-
  // -------------------------------------------------------------------------- \\
  // ---------------------------- Controller ---------------------------------- \\
  // -------------------------------------------------------------------------- \\
